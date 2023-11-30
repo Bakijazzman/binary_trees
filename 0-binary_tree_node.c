e09ab93aa27064884852a14a792d35347ed52752
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-30 17:27:21 +0100

    creates node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a new node
+ * @parent: Parent node
+ * @value: value of the node
+ * Return: pointer to the  node
+ */
+binary_tree_t *binary_tree_node (binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return(new);
+}
